9237bca8fe725d70b9b4f13d93596a33391bf3fa
小满1221 <im@manjiuqi.com>
2025-12-13 02:49:15 +0800

    Delete Files & Modify Some page

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/12/hello-world/"];function toRandomPost(){
+var posts=["2025/12/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
